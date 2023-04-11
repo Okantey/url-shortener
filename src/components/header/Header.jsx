cc53bf021107804e01be0d377e07a5e6d67ab59e
nii_okantey <nunoogideon19@gmail.com>
2023-04-11 20:32:51 +0000

    hero done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import Navbar from "../navbar/Navbar";
 import Button from "../button/Button";
+import hero from "../../assets/illustration-working.svg";
 
 const Header = () => {
   return (
@@ -10,13 +11,18 @@ const Header = () => {
       <section className="hero">
         <div className="hero__left">
           <h1 className="hero__block">More than just shorter links</h1>
-          <p className="hero__description">
+          <p
+            style={{ color: "var(--Grayish-Violet)", width: "80%" }}
+            className="hero__description"
+          >
             Build your brand’s recognition and get detailed insights on how your
             links are performing.
           </p>
           <Button name="Get Started" />
         </div>
-        <div className="hero__right"></div>
+        <div className="hero__right">
+          <img src={hero} alt="hero__image" />
+        </div>
       </section>
     </header>
   );
